1428bfd3a921525c5e2074cc9543f9be6c398115
Joon <wnstjq0915@gmail.com>
2023-07-18 00:48:08 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Api
+
+from resources.photo import PhotoResource, PhotoReviewResource
+from resources.search import NewsSearchResource
+from resources.translate import TranslateResource
+
+app = Flask(__name__)
+
+api = Api(app)
+
+api.add_resource( PhotoResource ,  '/photo')
+api.add_resource( PhotoReviewResource  , '/review')
+api.add_resource( NewsSearchResource  , '/search/news')
+api.add_resource( TranslateResource , '/translate')
+
+if __name__ == '__main__':
+    app.run()
